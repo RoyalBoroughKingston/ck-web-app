a4a85dc476aa865999072fd2eb99cbe79d017ec0
jaderyan <jadeelizabethryan@outlook.com>
2020-01-31 11:24:20 +0000

    banner component

---
+++ src/components/UnsupportedBrowser/UnsupportedBrowser.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './UnsupportedBrowser.scss';
+
+export const UnsupportedBrowser = () => (
+    <div className="unsupported">
+        This browser is unsupported and the Connected Kingston website may not perform as intended. Please upgrade your browser to experience the site at its best.
+    </div>
+)
